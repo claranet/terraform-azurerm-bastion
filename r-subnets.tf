5e19bca436bb9f0d4c08f49afd11f0cece23026d
OneGit service account - Renovate <fr-ops4ops+bot-renovate@fr.clara.net>
2024-05-03 14:18:08 +0000

    chore(deps): update dependency claranet/subnet/azurerm to v7

--- r-subnets.tf
+++ r-subnets.tf
@@ -1,6 +1,6 @@
 module "subnet_bastion" {
   source  = "claranet/subnet/azurerm"
-  version = "6.3.0"
+  version = "7.0.0"
 
   environment    = var.environment
   location_short = var.location_short
